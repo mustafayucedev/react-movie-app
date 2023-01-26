79759739cfadd85a6e05490af58dbb3298a8bff9
Mustafa Yüce <75587873+mustafayucedev@users.noreply.github.com>
2023-01-26 13:44:22 +0300

    update folder

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -9,7 +9,10 @@ function MovieCard( {data} ) {
       <div className={style.item}>
         <div className={style.top}>
             <div className={style.poster}>
-              <img src={data.poster_path ? API_IMG + data.poster_path : "https://images.unsplash.com/photo-1540224871915-bc8ffb782bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt={data.title} />
+              <img 
+                src={data.poster_path ? API_IMG + data.poster_path : "https://images.unsplash.com/photo-1540224871915-bc8ffb782bdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} 
+                alt={data.title} 
+              />
             </div>
             <div className={style.average}> {data.vote_average} </div>
             <div className={style.overview}>
